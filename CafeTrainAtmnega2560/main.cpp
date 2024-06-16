a1dacde2ff7b5572738a8b7e71c778efcd53f725
mihan2007 <mihan2007@yandex.ru>
2024-06-16 20:14:06 +0300

    seaparted tabales

--- CafeTrainAtmnega2560/main.cpp
+++ CafeTrainAtmnega2560/main.cpp
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdbool.h> // Добавлен заголовочный файл для использования типа данных bool
+#include <stdbool.h>
 
 #define TableButton_1 PF0
 #define TableButton_2 PF1
@@ -19,35 +19,34 @@
 #define TableLED_5 PK4
 
 #define MoveForwarLED PK6
-#define MoveBackwardLED PK7 
+#define MoveBackwardLED PK7
 
-#define ReversPin PA1	  //D23
+#define ReversPin PA1
 
-#define RailSwitchL_1 PA2 //D24
-#define RailSwitchR_1 PA3 //D25
+#define RailSwitchL_1 PA2
+#define RailSwitchR_1 PA3
 
-#define RailSwitchL_2 PA4 //D26
-#define RailSwitchR_2 PA5 //D27
+#define RailSwitchL_2 PA4
+#define RailSwitchR_2 PA5
 
-#define RailSwitchL_3 PA6 //D228
-#define RailSwitchR_3 PA7 //D229
+#define RailSwitchL_3 PA6
+#define RailSwitchR_3 PA7
 
-#define RailSwitchL_4 PC5 //D31
-#define RailSwitchR_4 PC6 //D32
+#define RailSwitchL_4 PC5
+#define RailSwitchR_4 PC6
 
-#define EndWaySensor PC2 //D35
+#define EndWaySensor PC2
 
-#define TableSensor PL0 // D49
+#define TableSensor PL0
+#define StartPointSencor PL1
+#define GroundRail PL6
+#define KitchenSensor PL7
 
-#define StartPointSencor PL1 //D48
-
-#define KitchenSensor PL7 //42
-
-#define SwitchTable_1 PG1 //D40
-#define SwitchTable_2 PG2 //D39
-#define SwitchTable_3 PD7 //D38
-#define SwitchTable_4 PC0 //D37
-#define SwitchTable_5 PC1 //D36
+#define SwitchTable_1 PG1
+#define SwitchTable_2 PG2
+#define SwitchTable_3 PD7
+#define SwitchTable_4 PC0
+#define SwitchTable_5 PC1
 
 #define Gear_1_Pin PD3
 #define Gear_2_Pin PD2
@@ -58,73 +57,79 @@ bool IsTrainMoving = false;
 bool IsTableChosen = false;
 bool IsTrainArrivedToTable = false;
 
-int ChoisenTableNumber = 0; 
+bool previousButtonState[8] = {false};
+
+void SwithOffAllTables()
+{
+	PORTG &= ~((1 << SwitchTable_1) | (1 << SwitchTable_2));
+	PORTD &= ~(1 << SwitchTable_3);
+	PORTC &= ~((1 << SwitchTable_4) | (1 << SwitchTable_5));
+}
 
 void AdjustWay(int ChosenTable)
 {
-	ChoisenTableNumber = ChosenTable;
+	SwithOffAllTables();
 	
 	int DelayBWRailSwitch = 1000;
 	
 	IsTableChosen = true;
 	
-	switch (ChosenTable) 
+	switch (ChosenTable)
 	{
 		case 1:
-
 		PORTA |= (1 << RailSwitchL_1);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchL_1);
 		
-
+		PORTG |= (1 << SwitchTable_1);
 		break;
 		
 		case 2:
 		PORTA |= (1 << RailSwitchR_1);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_1);
-	
+		
 		PORTA |= (1 << RailSwitchL_2);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchL_2);
 		
+		PORTG |= (1 << SwitchTable_2);
 		break;
 		
 		case 3:
-
 		PORTA |= (1 << RailSwitchR_1);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_1);
-	
+		
 		PORTA |= (1 << RailSwitchR_2);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_2);
-	
+		
 		PORTA |= (1 << RailSwitchL_3);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchL_3);
 		
-		
+		PORTD |= (1 << SwitchTable_3);
 		break;
 		
 		case 4:
-
 		PORTA |= (1 << RailSwitchR_1);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_1);
-	
+		
 		PORTA |= (1 << RailSwitchR_2);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_2);
-	
+		
 		PORTA |= (1 << RailSwitchR_3);
 		_delay_ms(DelayBWRailSwitch);
 		PORTA &= ~ (1 << RailSwitchR_3);
-	
+		
 		PORTC |= (1 << RailSwitchL_4);
 		_delay_ms(DelayBWRailSwitch);
 		PORTC &= ~ (1 << RailSwitchL_4);
-
+		
+		PORTC |= (1 << SwitchTable_4);
 		break;
 		
 		case 5:
@@ -143,40 +148,23 @@ void AdjustWay(int ChosenTable)
 		PORTC |= (1 << RailSwitchR_4);
 		_delay_ms(DelayBWRailSwitch);
 		PORTC &= ~ (1 << RailSwitchR_4);
-					
+		
+		PORTC |= (1 << SwitchTable_5);
 		break;
 		
 		default:
-	
 		break;
 	}
 }
 
 void TurnOnButtonLED(int ChosenTable)
 {
-	// Выключаем все светодиоды
-	PORTK &= ~((1 << TableLED_1) | (1 << TableLED_2) | (1 << TableLED_3) | (1 << TableLED_4) | (1 << TableLED_5)); // | (1 << TableLED_6));
+	PORTK &= ~((1 << TableLED_1) | (1 << TableLED_2) | (1 << TableLED_3) | (1 << TableLED_4) | (1 << TableLED_5));
 
-	// Включаем выбранный светодиод если был передан номер отличный от нуля, если ноль то все свтодиоды просто выключаются
 	if (ChosenTable != 0)
 	{
-			PORTK |= (1 << (ChosenTable - 1));
+		PORTK |= (1 << (ChosenTable - 1));
 	}
-	
-}
-
-void MakeTrainMove() 
-{	
-	int stepDelay = 1000; 
-	PORTD |= (1 << Gear_1_Pin);
-	_delay_ms(stepDelay);
-	PORTD |= (1 << Gear_2_Pin);
-	_delay_ms(stepDelay);
-	PORTD |= (1 << Gear_3_Pin);
-	_delay_ms(stepDelay);
-	PORTD |= (1 << Gear_4_Pin);	
-	
-	IsTrainMoving = true;
 }
 
 void SetLEDMove (bool forwardLED, bool backwardLED)
@@ -198,12 +186,12 @@ void StopTrain()
 	IsTrainMoving = false;
 	PORTD &=~ ((1 << Gear_1_Pin) | (1 << Gear_2_Pin) | (1 << Gear_3_Pin) | (1 << Gear_4_Pin));
 	PORTA &=~ (1 << ReversPin);
+	PORTL &=~ (1 << GroundRail);
 	SetLEDMove(0,0);
 }
 
 void MoveTrain(bool direction)
-{	
-
+{
 	if (direction == 1 && PINA & (1 << ReversPin))
 	{
 		StopTrain();
@@ -213,121 +201,130 @@ void MoveTrain(bool direction)
 	if (direction == 0 && !(PINA & (1 << ReversPin)))
 	{
 		StopTrain();
+		SetLEDMove(0,1);
 		PORTA |= (1 << ReversPin);
 	}
 
+	PORTL |= (1 << GroundRail);
+	int stepDelay = 1000;
+	PORTD |= (1 << Gear_1_Pin);
+	_delay_ms(stepDelay);
+	PORTD |= (1 << Gear_2_Pin);
+	_delay_ms(stepDelay);
+	PORTD |= (1 << Gear_3_Pin);
+	_delay_ms(stepDelay);
+	PORTD |= (1 << Gear_4_Pin);
 
-	MakeTrainMove();
+	IsTrainMoving = true;
 }
 
-void SlowMode () 
+void SlowMode ()
 {
-	int stepDelay = 50; 
+	int stepDelay = 50;
 	PORTD &= ~ (1 << Gear_1_Pin);
 	_delay_ms(stepDelay);
 	PORTD &= ~ (1 << Gear_2_Pin);
 	_delay_ms(stepDelay);
 	PORTD |= (1 << Gear_3_Pin);
 	_delay_ms(stepDelay);
-	PORTD |= (1 << Gear_4_Pin);	
+	PORTD |= (1 << Gear_4_Pin);
 }
-	
+
+bool isButtonPressed(uint8_t buttonPin, bool *previousState)
+{
+	if (!(PINF & (1 << buttonPin)))
+	{
+		if (!(*previousState))
+		{
+			*previousState = true;
+			_delay_ms(50); // Дебаунсинг задержка
+			return true;
+		}
+	}
+	else
+	{
+		*previousState = false;
+	}
+	return false;
+}
+
 int main(void)
 {
-	// PF Настройка портов кнопок переключения столов как вход
-	DDRF = 0x00; // Установка всех битов порта PF в 0 (входы)
-	//Установка PB в качестве входов кнопок
-	DDRB &= ~(1 << StopButton); // | (1 << MoveTrainButton));
-
-	// Настройка портов PK как выходов кнопка подсветки выбранного стола
-	DDRK = 0xFF; // Установка всех битов порта PK в 1 (выходы)
-	PORTK = 0x00; // Начальное состояние выходов порта PK (выключено)
-
-	// Настройка портов Стрелок как выходов
+	DDRF = 0x00;
+	DDRB &= ~(1 << StopButton);
+	DDRK = 0xFF;
+	PORTK = 0x00;
 	DDRA |= (1 << RailSwitchL_1) | (1 << RailSwitchR_1) | (1 << RailSwitchL_2) | (1 << RailSwitchR_2) | (1 << RailSwitchL_3) | (1 << RailSwitchR_3) | (1 << ReversPin);
-	DDRC |= (1 << RailSwitchL_4) | (1 << RailSwitchR_4); // | (1 << EndWaySensor); //| (1 << RailSwitchR_5); // | (1 << RailSwitchL_6) | (1 << RailSwitchR_6);
-	DDRD |= (1 << Gear_1_Pin) | (1 << Gear_2_Pin) | (1 << Gear_3_Pin) | (1 << Gear_4_Pin);
-	
-	// Насстройка порта выключателя концевого выключателя
+	DDRC |= (1 << RailSwitchL_4) | (1 << RailSwitchR_4) | (1 << SwitchTable_4) | (1 << SwitchTable_5);
+	DDRD |= (1 << Gear_1_Pin) | (1 << Gear_2_Pin) | (1 << Gear_3_Pin) | (1 << Gear_4_Pin) | (1 << SwitchTable_3);
+	DDRG |= (1 << SwitchTable_1) | (1 << SwitchTable_2);
 	DDRC &= ~(1 << EndWaySensor);
-	
-	// Настраиваем порты датчика перехода на тихий ход
-	
-	DDRL &= ~ (1 << TableSensor);
-
+	DDRL &= ~((1 << TableSensor) | (1 << KitchenSensor));
+	DDRL |= (1 << GroundRail);
 
 	while (1)
 	{
-		
 		if (IsTrainMoving == false)
 		{
-			// Проверяем нажатие кнопок и выполняем соответствующие действия
-			if (!(PINF & (1 << TableButton_1)))
+			if (isButtonPressed(TableButton_1, &previousButtonState[0]))
 			{
-				TurnOnButtonLED(1); // Включение светодиода для стола
-				AdjustWay(1);       // Передача указателя на порт для стола 1
+				TurnOnButtonLED(1);
+				AdjustWay(1);
 			}
 
-			if (!(PINF & (1 << TableButton_2)))
+			if (isButtonPressed(TableButton_2, &previousButtonState[1]))
 			{
-				TurnOnButtonLED(2); // Включение светодиода для стола
-				AdjustWay(2);       // Передача указателя на порт для стола 1
+				TurnOnButtonLED(2);
+				AdjustWay(2);
 			}
 
-			if (!(PINF & (1 << TableButton_3)))
+			if (isButtonPressed(TableButton_3, &previousButtonState[2]))
 			{
-				TurnOnButtonLED(3); // Включение светодиода для столатакое
-				AdjustWay(3);       // Передача указателя на порт для стола 1
+				TurnOnButtonLED(3);
+				AdjustWay(3);
 			}
 
-			if (!(PINF & (1 << TableButton_4)))
+			if (isButtonPressed(TableButton_4, &previousButtonState[3]))
 			{
-				TurnOnButtonLED(4); // Включение светодиода для стола
-				AdjustWay(4);       // Передача указателя на порт для стола 1
+				TurnOnButtonLED(4);
+				AdjustWay(4);
 			}
 
-			if (!(PINF & (1 << TableButton_5)))
+			if (isButtonPressed(TableButton_5, &previousButtonState[4]))
 			{
-				TurnOnButtonLED(5); // Включение светодиода для стола
-				AdjustWay(5);       // Передача указателя на порт для стола 1
-			}	
+				TurnOnButtonLED(5);
+				AdjustWay(5);
+			}
 		}
 		
-		// Если была нажата кнопка движения поезда вперед, проверяется условие был ли выбран стол и не находится ли локомотив вконце пути
 		if (!(PINF & (1 << MoveForwardButton)) && IsTableChosen == true && IsTrainArrivedToTable == false)
 		{
 			SetLEDMove(1,0);
-			MoveTrain(1);	
+			MoveTrain(1);
 		}
 		
-		
 		if (!(PINF & (1 << MoveBackwardButton)) && IsTableChosen == true)
-		{		
-			SetLEDMove(0,1);
+		{
 			MoveTrain(0);
 			IsTrainArrivedToTable = false;
 		}
 
-		// Если нажата кнопка остановки 
 		if (!(PINF & (1 << StopButton)))
 		{
-			SetLEDMove(0,0);
 			StopTrain();
 		}
 		
-		// Если локомотив наехал на датчик окончания пути на столе, проверяется условия не включен ли реверс дивжения 
 		if (!(PINC & (1 << EndWaySensor)) && !(PINA & (1 << ReversPin)))
 		{
 			StopTrain();
 			IsTrainArrivedToTable = true;
 		}
-		// если поезд едет вперед и наезжает на датчик замедления
+		
 		if (!(PINL & (1 << TableSensor)) && !(PINA & (1 << ReversPin)))
 		{
 			SlowMode();
-					}
+		}
 		
-		// Если поезд наехал на замеделния при подъезде к кухне 
 		if (!(PINL & (1 << KitchenSensor)) && PINA & (1 << ReversPin))
 		{
 			SlowMode();
@@ -335,16 +332,13 @@ int main(void)
 			IsTableChosen = false;
 		}
 		
-		// Если локомотив наехал на датчик выключния поезда
 		if (!(PINL & (1 << StartPointSencor)) && PINA & (1 << ReversPin))
 		{
 			StopTrain();
-			//TurnOnButtonLED(0);
-		} 		
-				
-		_delay_ms(50);		
+		}
+		
+		_delay_ms(50);
 	}
 
 	return 0;
-	
 }
